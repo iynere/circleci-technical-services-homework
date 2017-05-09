dfdedb6e8de9bdcf0cfc1dff6eedd712361b8591
isar0se <itsr053@gmail.com>
2017-05-09 14:18:15 -0400

    gh pages

--- src/app.test.js
+++ src/app.test.js
@@ -3,17 +3,17 @@ import {shallow} from 'enzyme'
 import App from './app'
 
 test('text does not display before clicking button', () => {
-	const app = shallow(<App />),
-		text = 'Hello there, here is some text, check it out!'
+  const app = shallow(<App />),
+    text = 'Hello there, here is some text, check it out!'
 
-	expect(app.contains(text)).toEqual(false)
+  expect(app.contains(text)).toEqual(false)
 })
 
 test('text does display after clicking button', () => {
-	const app = shallow(<App />),
-		text = 'Hello there, here is some text, check it out!'
+  const app = shallow(<App />),
+    text = 'Hello there, here is some text, check it out!'
 
-	app.find('.btn').simulate('click')
+  app.find('.btn').simulate('click')
 
-	expect(app.contains(text)).toEqual(true)
+  expect(app.contains(text)).toEqual(true)
 })
