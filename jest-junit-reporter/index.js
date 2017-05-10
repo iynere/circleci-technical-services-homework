ed02097d9afc0d960be03461406eff50e1d7041a
avena <avena@hrdwre3.local>
2017-05-10 17:46:06 -0400

    got rid of es6 in jest reporter module

---
+++ jest-junit-reporter/index.js
@@ -0,0 +1,13 @@
+var Testsuites = require('./src/Testsuites');
+var xml = require('xml');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = (results) => {
+  var out = process.env.TEST_REPORT_PATH || process.cwd();
+  var filename = process.env.TEST_REPORT_FILENAME || 'test-report.xml';
+  var testSuites = new Testsuites(results);
+  var data = xml(testSuites, { declaration: true, indent: '  ' });
+  fs.writeFileSync(path.join(out, filename), data);
+  return results;
+};
