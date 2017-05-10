220b9b4abd245b9da845377aefd2c4523148b2d6
avena <avena@hrdwre3.local>
2017-05-10 17:32:16 -0400

    got rid of some es6

--- scripts/test.js
+++ scripts/test.js
@@ -9,8 +9,8 @@ process.env.PUBLIC_URL = '';
 // https://github.com/motdotla/dotenv
 require('dotenv').config({silent: true});
 
-const jest = require('jest');
-const argv = process.argv.slice(2);
+var jest = require('jest');
+var argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
